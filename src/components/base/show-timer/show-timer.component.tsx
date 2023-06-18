a8b3513e0f81e49add7e97eddc23c71041d4e277
ruba <ruba@gmail.com>
2023-06-18 13:13:05 +0300

    smth

--- src/components/base/show-timer/show-timer.component.tsx
+++ src/components/base/show-timer/show-timer.component.tsx
@@ -18,10 +18,10 @@ interface IProps {
     elementRef?: any,
 }
 
-const ShowTimer = ({ children, timeout, animationType = AnimationType.FADE_UP, elementRef }: IProps) => {
+const ShowTimer = ({ children, timeout, animationType: animationtype = AnimationType.FADE_UP, elementRef }: IProps) => {
     const isInView = useScroll(elementRef);
     const shown = useTimer(timeout)
-    return React.cloneElement(children, { className: `${children?.props?.className} hidable${(!shown || (elementRef != null && !isInView)) ? ` ${animationType}` : ``}` })
+    return React.cloneElement(children, { className: `${children?.props?.className} hidable${(!shown || (elementRef != null && !isInView)) ? ` ${animationtype}` : ``}` })
 }
 
 export default ShowTimer;
